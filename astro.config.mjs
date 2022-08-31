38b1e002f0dceacbadcd9261a8db9540ce3ad806
Sanskar Tiwari <sanskar.mk2@gmail.com>
2022-08-31 15:49:15 +0530

    🌱 fresh with deps

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [tailwind(), image(), mdx(), sitemap(), react()],
+});
